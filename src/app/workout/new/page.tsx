320c6a88ceb4303519bb62a9c1be292adaecd089
Julius Reade <julius.reade@gmail.com>
2025-04-01 20:44:38 +1100

    feat: add base pages

---
+++ src/app/workout/new/page.tsx
@@ -0,0 +1,9 @@
+export default function NewWorkout() {
+  return (
+    <div>
+      <h1>Workout</h1>
+      <input type="text" />
+      {/* <button onClick={}>Save</button> */}
+    </div>
+  );
+}
---
+++ src/app/workout/page.tsx
@@ -0,0 +1,14 @@
+export default function Workout() {
+  return (
+    <div>
+      <h1>Workout</h1>
+      <div>
+        {/* {workoutList.map(workout => {
+          <p>{workout.name}</p>
+        })} */}
+      </div>
+
+      <button onClick={}>Add workout</button>
+    </div>
+  );
+}
